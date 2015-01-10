4d3742d8eff77ce87e24296d752d8b8f04b92156
jeswang <wangyi724@gmail.com>
2015-01-11 01:13:58 +0800

    init deck-manager.

---
+++ Hearthstone-Deck-Tracker/Controllers/DeckManangerController.h
@@ -0,0 +1,13 @@
+//
+//  DeckManangerController.h
+//  Hearthstone-Deck-Tracker
+//
+//  Created by jeswang on 15/1/11.
+//  Copyright (c) 2015年 rainy. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface DeckManangerController : NSViewController
+
+@end
---
+++ Hearthstone-Deck-Tracker/Controllers/DeckManangerController.m
@@ -0,0 +1,22 @@
+//
+//  DeckManangerController.m
+//  Hearthstone-Deck-Tracker
+//
+//  Created by jeswang on 15/1/11.
+//  Copyright (c) 2015年 rainy. All rights reserved.
+//
+
+#import "DeckManangerController.h"
+
+@interface DeckManangerController ()
+
+@end
+
+@implementation DeckManangerController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do view setup here.
+}
+
+@end
