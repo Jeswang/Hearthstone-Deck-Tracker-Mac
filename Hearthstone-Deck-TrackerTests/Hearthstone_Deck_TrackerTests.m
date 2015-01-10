7b78b7f8698e9fad5333cdd0f461ad453c6d4390
jeswang <wangyi724@gmail.com>
2015-01-11 00:40:07 +0800

    Change git path to parent directory

---
+++ Hearthstone-Deck-TrackerTests/Hearthstone_Deck_TrackerTests.m
@@ -0,0 +1,40 @@
+//
+//  Hearthstone_Deck_TrackerTests.m
+//  Hearthstone-Deck-TrackerTests
+//
+//  Created by jeswang on 15/1/9.
+//  Copyright (c) 2015年 rainy. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import <XCTest/XCTest.h>
+
+@interface Hearthstone_Deck_TrackerTests : XCTestCase
+
+@end
+
+@implementation Hearthstone_Deck_TrackerTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
---
+++ Podfile
@@ -0,0 +1,8 @@
+source 'https://github.com/CocoaPods/Specs.git'
+
+platform :osx
+inhibit_all_warnings!
+
+pod 'ReactiveCocoa'
+pod 'Realm'
+
